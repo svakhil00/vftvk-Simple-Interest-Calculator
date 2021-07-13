9cb76db53e95849931f1d865cfde27048b11a2bc
svakhil00 <svakhil00@gmail.com>
2021-07-13 18:27:26 -0400

    added functionality

--- script.js
+++ script.js
@@ -1,6 +1,25 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
+function simpleInterest(){
+    
+    var amount = document.getElementById("amount");
+    var rate = document.getElementById("interestrate");
+    var time = document.getElementById("years");
+
+    if(amount.value == ""){
+        window.alert("Please enter an amount")
+        amount.focus();
+        return false;
+    }
+    if(amount.value < 1){
+        window.alert("Please enter a positive number");
+        amount.focus();
+        return false;
+    }
+    
+    var total = (1 + rate.value * Number(time.value)) * amount.value;
+    document.getElementsByClassName("div1")[0].hidden = false;
+    document.getElementsByClassName("highlight1")[0].innerHTML = amount.value;
+    document.getElementsByClassName("highlight2")[0].innerHTML = rate.value;
+    document.getElementsByClassName("highlight3")[0].innerHTML = total;
+    document.getElementsByClassName("highlight4")[0].innerHTML = new Date().getFullYear() + Number(time.value);
     
 }
-        
